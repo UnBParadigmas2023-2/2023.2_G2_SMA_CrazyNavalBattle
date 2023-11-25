3796bf0d22fe7a96f5fdd9cf4bec751eaf05dd43
AdneMoretti <morettiadne1@gmail.com>
2023-11-24 23:58:18 -0300

    Iniciando source

---
+++ src/ContraMorteiro.py
@@ -0,0 +1,12 @@
+class ContraMorteiro(): 
+    def __init__(self, posicao, qtd):
+        self.posicao = posicao
+        self.qtd = qtd
+
+    def defend(): 
+        pass
+
+    def attack(): 
+        pass
+
+
---
+++ src/model.py
@@ -0,0 +1,11 @@
+class NavalBattle(): 
+    def __init__(
+            self, 
+            num_cruzador, 
+            num_torpedeiro, 
+            num_morteiro, 
+            num_contra_torpedeiro, 
+            num_contra_morteiro
+        ): 
+        pass
+
