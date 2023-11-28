aceb7c5e752303fe48ec268cf11ce60d42f79a45
luclopesr <luclopesr@gmail.com>
2023-11-28 19:38:39 -0300

    Incrementando no Contra Morteiro
    Co-authored-by: Adne Moretti <morettiadne1@gmail.com>

--- src/ContraMorteiro.py
+++ src/ContraMorteiro.py
@@ -19,3 +19,24 @@ class ContraMorteiro(BoatAgent):
 
     def base_range(self):
         return 5
+
+    def operate(self):
+        # Lógica específica do Morteiro durante a operação
+        enemies_in_range = list(self._enemies_in_range())
+        if enemies_in_range:
+            target = self.model.random.choice(enemies_in_range)
+            target.receive_damage(self.calculate_damage())
+
+    def move(self):
+        # Lógica específica do movimento do Morteiro (se houver)
+        pass
+
+    def calculate_damage(self):
+        # Calcula o dano total, levando em consideração o dano base e quaisquer modificadores adicionais
+        return self.base_damage()
+
+    def receive_damage(self, damage):
+        # Reduz os pontos de vida com base no dano recebido
+        self._health_points -= damage
+        if self._health_points <= 0:
+            self.model.grid.remove_agent(self)
