40b818a9a038050dcd488e72522c69fb04223abe
Guilherme Puida <guilherme@puida.xyz>
2023-12-01 08:10:17 -0300

    ContraMorteiro is now strong against Morteiro

--- src/ContraMorteiro.py
+++ src/ContraMorteiro.py
@@ -15,9 +15,6 @@ class ContraMorteiro(BoatAgent):
 
     def base_damage(self):
         return 1
-
-    def step(self):
-        super().step()
         
     def base_health_points(self):
         return 10
@@ -25,13 +22,5 @@ class ContraMorteiro(BoatAgent):
     def base_range(self):
         return 5
 
-    # def move(self):
-    #     # Coloquei logica de mover nas diagonais (primeira que encontrar vazia)
-    #     a, b = self.pos
-    #     moves = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
-    #     for x, y in moves: 
-    #         new_position = (a+x, b+y)
-    #         if self.grid.is_cell_empty(new_position):
-    #             self.pos = new_position
-    #             self.model.grid.move_agent(self, self.pos)
-    #             return 
+    def strong_against(self):
+        return {"Morteiro"}
